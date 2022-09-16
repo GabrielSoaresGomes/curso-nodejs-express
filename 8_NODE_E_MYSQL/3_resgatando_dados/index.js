b7d2f1c5dbaa36dc824deaec004f504555239164
Gabriel Dragon <gabrielsoares221@gmail.com>
2022-09-16 00:43:12 -0300

    Iniciando integração com mysql!

---
+++ 8_NODE_E_MYSQL/3_resgatando_dados/index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.post('/books/insertbook', ((req, res) => {
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`
+    conn.query(sql, (err) => {
+        if (err) {
+            console.log(err)
+        }
+
+        res.redirect('/books')
+    })
+}))
+
+app.get('/books', (req, res) => {
+    const sql = "SELECT * FROM books"
+
+    conn.query(sql, (err, data) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const books = data
+        res.render('books', {books})
+    })
+})
+
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql'
+})
+
+conn.connect((err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log("Conectado ao banco de dados!")
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Aplicação rondando na porta ${PORT}`)
+})
